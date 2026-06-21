4fd39a74a48a6973ace314a12f45fa9992e98fe1
sharifmb92-bit <270324566+sharifmb92-bit@users.noreply.github.com>
2026-06-21 20:52:13 +0200

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,16 @@
+const cacheName = 'menorca-cash-mb-v2';
+const assets = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+});
